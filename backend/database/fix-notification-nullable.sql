2cf1e7c8ac45e7de3e3d71cdeaebae42a5914b3e
Pramod Shinde <pramodshinde2005@gmail.com>
2026-02-19 00:24:23 +0530

    chore: remove debug, test, and instruction files

--- backend/database/fix-notification-nullable.sql
+++
@@ -1,7 +0,0 @@
--- Fix notification table: make request_id nullable so event notifications work
-ALTER TABLE notification MODIFY COLUMN request_id INT NULL;
-
--- Also make link column nullable if not already
-ALTER TABLE notification MODIFY COLUMN link VARCHAR(500) NULL;
-
-SELECT 'Notification table fixed - request_id is now nullable' AS Status;
